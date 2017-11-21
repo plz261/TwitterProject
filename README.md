c97c041255eeebd297b8c7e3716fa1c6574bf44f
Mack Renberg <mack.renberg@pioneer.com>
2017-11-20 20:11:11 -0600

    initial work

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# TwitterProject
+1. change the emoji.json path in src/EmojiFunctions.scala to fit your local machine
+-this version of scala(2.11.x) doesn't have scala.io.Source.fromResource to load files from
+
+2. download sbt to run project
+
+3. enter "sbt"
+
+4. enter "run main"
---
+++ twitterProject/src/main/scala/StreamBuild.scala
@@ -0,0 +1,30 @@
+package org.twitter4j
+
+import twitter4j.TwitterFactory
+import twitter4j.TwitterStreamFactory
+
+trait TwitterInstance {
+  val twitter = new TwitterFactory().getInstance
+}
+
+class StreamBuild {
+  def twitterSample = {
+    val twitterUtilities = new TwitterUtil()
+    val twitterAuth = new TwitterAuth()
+    val twitterStream = new TwitterStreamFactory(twitterAuth.twitterAuth()).getInstance
+    val twitter4jListener = twitterUtilities.simpleStatusListener
+    twitterStream.addListener(twitter4jListener)
+    twitterStream.sample
+  }
+}
+
+class TwitterAuth {
+  def twitterAuth() = {
+    val config = new twitter4j.conf.ConfigurationBuilder()
+    config.setOAuthConsumerKey("")
+    config.setOAuthConsumerSecret("")
+    config.setOAuthAccessToken("")
+    config.setOAuthAccessTokenSecret("")
+    config.build
+  }
+}
